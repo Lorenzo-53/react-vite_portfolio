25501f33a7fb6702723197e17e5865845a80cc29
Lorenzo-53 <anshsrivstava835@gmail.com>
2025-10-30 22:21:48 +0530

    first commit

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {socialImgs} from "../constants/index.js";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-container">
+                <div className=" flex flex-col justify-center md:items-start items-center">
+                    <a href="/">Visit My Vlog!</a>
+                </div>
+                <div className="socials">
+                    {socialImgs.map((img) => (
+                        <a className="icon" target="_blank" href={img.url} key={img.url}>
+                            <img src={img.imgPath} alt=""/>
+                        </a>
+                        ))}
+                </div>
+                <div className=" flex flex-col justify-center ">
+                <p className="text-center md:text-end">
+                    ©{new Date().getFullYear()}Ansh | Lorenzo. All rights reserved.
+                </p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+export default Footer
