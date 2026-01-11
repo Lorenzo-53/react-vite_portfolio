3c3d3ccf2800e4c9ef0c8431731f0f506db669ee
Lorenzo-53 <anshsrivstava835@gmail.com>
2026-01-11 20:23:09 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
         <>
             <NavBar/>
             <Hero/>
-            {/* <ShowcaseSection/> */}
+            <ShowcaseSection/>
             <LogoSection/>
             <FeatureCards/>
             {/*<ExperienceSection/>*/}
