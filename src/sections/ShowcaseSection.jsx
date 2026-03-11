3fb354a4d9d5c869b14786af005980aa515e80ea
Lorenzo-53 <anshsrivstava835@gmail.com>
2026-03-12 00:01:21 +0530

    production

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -51,7 +51,7 @@ const ShowcaseSection = () => {
                     {/*left*/}
                     <div className="first-project-wrapper" ref={project1Ref}>
                         <div className="image-wrapper">
-                            <img src="/images/project1.png" alt="Ryde"
+                            <img src="/images/project1.webp" alt="Ryde"
                             />
                         </div>
                         <div className="text-content">
@@ -71,13 +71,13 @@ const ShowcaseSection = () => {
 
                         <div className="project" ref={project2Ref}>
                             <div className="image-wrapper bg-[#ffefdb]">
-                                <img src="/images/project2.png" alt="Library Management"/>
+                                <img src="/images/project2.webp" alt="Library Management"/>
                             </div>
                             <h2>Library Management Platform</h2>
                         </div>
                         <div className="project" ref={project3Ref}>
                             <div className="image-wrapper bg-[#ffe7eb]">
-                                <img src="/images/project3.png" alt="YC  Directory"/>
+                                <img src="/images/project3.webp" alt="YC  Directory"/>
                             </div>
                             <h2>YC Directory - A Startup Showcase App </h2>
                         </div>
